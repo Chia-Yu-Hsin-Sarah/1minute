13801beb4ed009aeda290497cde361958f5ee8c3
Chia Yu Hsin Sarah <sarah.chia8795@gmail.com>
2023-09-07 20:12:14 +0800

    Initial Commit

---
+++ 1minute/_minuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  _minuteApp.swift
+//  1minute
+//
+//  Created by Sarah Chia on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct _minuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
